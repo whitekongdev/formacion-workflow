8f87940f97be5f28fc56adac64d1eee034d9f14b
SamuelWK <samuel@whitekongsoftware.com>
2022-07-18 11:27:04 +0100

    feat(init):Initial--4

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function isValid(params) {
 const h = "Tutankamon"          
 const f = "Cerbero"          
 const x = "Hercules"          
+const y = "MoonKnight"          
 
 if (isValid()) {
     console.log('holaass')
