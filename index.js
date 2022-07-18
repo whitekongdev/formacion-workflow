3e5a33123b12d64d21d2af6f237a83d9866be677
SamuelWK <lucas.alvarez-acosta@whitekongsoftware.com>
2022-07-18 11:18:01 +0100

    feat(init):InitialCommit

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ function isValid(params) {
   console.log("holass")
 }
 
-          
+const h = "Tutankamon"          
 
 if (isValid()) {
     console.log('holaass')
+}
+
+if (h) {
+  console.log('Egipto')
 }
